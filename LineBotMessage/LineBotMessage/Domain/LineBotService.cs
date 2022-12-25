57e1eed9f2e6f3d2ba1ec57ac7a1e68860b3221b
soulkey <tyler930030@gmail.com>
2022-12-26 01:20:49 +0800

    2022/12/26

--- LineBotMessage/LineBotMessage/Domain/LineBotService.cs
+++ LineBotMessage/LineBotMessage/Domain/LineBotService.cs
@@ -8,7 +8,11 @@ using Newtonsoft.Json;
 using System.Web;
 using Microsoft.AspNetCore.Mvc.Diagnostics;
 using LineBotMessage.Dtos.Messages;
+using Npgsql;
+using System;
 using static LineBotMessage.Dtos.BaseMessageDto;
+using NpgsqlTypes;
+using static Microsoft.Extensions.Logging.EventSource.LoggingEventSource;
 
 namespace LineBotMessage.Domain
 {
@@ -38,7 +42,8 @@ namespace LineBotMessage.Domain
                 {
                     case WebhookEventTypeEnum.Message:
                         if (eventObject.Message.Type == MessageTypeEnum.Text)
-                            await ReceiveMessageWebhookEvent(eventObject);
+                            getPostgresDate();
+                        await ReceiveMessageWebhookEvent(eventObject);
                         break;
                     case WebhookEventTypeEnum.Unsend:
                         Console.WriteLine($"使用者{eventObject.Source.UserId}在聊天室收回訊息！");
@@ -229,141 +234,176 @@ namespace LineBotMessage.Domain
         }
 
 
-            /// <summary>
-            /// 接收到回覆請求時，在將請求傳至 Line 前多一層處理(目前為預留)
-            /// </summary>
-            /// <param name="messageType"></param>
-            /// <param name="requestBody"></param>
-            public void ReplyMessageHandler<T>( ReplyMessageRequestDto<T> requestBody)
+        /// <summary>
+        /// 接收到回覆請求時，在將請求傳至 Line 前多一層處理(目前為預留)
+        /// </summary>
+        /// <param name="messageType"></param>
+        /// <param name="requestBody"></param>
+        public void ReplyMessageHandler<T>(ReplyMessageRequestDto<T> requestBody)
+        {
+            ReplyMessage(requestBody);
+        }
+
+        /// <summary>
+        /// 將回覆訊息請求送到 Line
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="request"></param>
+        public async void ReplyMessage<T>(ReplyMessageRequestDto<T> request)
+        {
+
+            try
             {
-                ReplyMessage(requestBody);
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", channelAccessToken); //帶入 channel access token
+                string? json = _jsonProvider.Serialize(request);
+                HttpRequestMessage? requestMessage = new HttpRequestMessage
+                {
+                    Method = HttpMethod.Post,
+                    RequestUri = new Uri(replyMessageUri),
+                    Content = new StringContent(json, Encoding.UTF8, "application/json")
+                };
+
+                HttpResponseMessage response = await client.SendAsync(requestMessage);
+                Console.WriteLine($"response.IsSuccessStatusCode = {response.IsSuccessStatusCode}");
             }
-
-            /// <summary>
-            /// 將回覆訊息請求送到 Line
-            /// </summary>
-            /// <typeparam name="T"></typeparam>
-            /// <param name="request"></param>
-            public async void ReplyMessage<T>(ReplyMessageRequestDto<T> request)
+            catch (Exception ex)
             {
-
-                try
-                {
-                    client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
-                    client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", channelAccessToken); //帶入 channel access token
-                    string? json = _jsonProvider.Serialize(request);
-                    HttpRequestMessage? requestMessage = new HttpRequestMessage
-                    {
-                        Method = HttpMethod.Post,
-                        RequestUri = new Uri(replyMessageUri),
-                        Content = new StringContent(json, Encoding.UTF8, "application/json")
-                    };
-
-                    HttpResponseMessage response = await client.SendAsync(requestMessage);
-                    Console.WriteLine($"response.IsSuccessStatusCode = {response.IsSuccessStatusCode}");
-                }
-                catch (Exception ex)
-                {
-                    Console.WriteLine("回復訊息失敗!\n" + ex.ToString());
-                }
-
-            }
-            static async Task<string> GetWeather()
-            {
-                try
-                {
-                    string result = "";
-                    string local = "新竹市";
-                    string Path = $"https://opendata.cwb.gov.tw/api/v1/rest/datastore/F-C0032-001?format=JSON&locationName={local}";
-                    client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("CWB-99A47F28-FFB9-467F-B4E9-6972DDCF3CD6");
-                    using HttpResponseMessage response = await client.GetAsync(Path);
-                    Console.WriteLine(response.ToString());
-
-                    if (response.IsSuccessStatusCode == true)
-                    {
-                        response.EnsureSuccessStatusCode();
-                        string responseBody = await response.Content.ReadAsStringAsync();
-                        WeatherReturn responseBodyJsonParse = JsonConvert.DeserializeObject<WeatherReturn>(responseBody);
-                        var StartTime = Convert.ToDateTime(responseBodyJsonParse.records.location[0].weatherElement[0].time[1].startTime).ToString("yyyy  MM / dd dddd HH:mm");
-                        var EndTime = Convert.ToDateTime(responseBodyJsonParse.records.location[0].weatherElement[0].time[1].endTime).ToString("yyyy  MM / dd dddd HH:mm");
-                        result = "新竹市12小時天氣預報" +
-                                                               Environment.NewLine + $"{StartTime}" +
-                                                               Environment.NewLine + $"{EndTime}" +
-                                                               Environment.NewLine + $"天氣狀態:{responseBodyJsonParse.records.location[0].weatherElement[0].time[0].parameter.parameterName}" +
-                                                               Environment.NewLine + $"降雨機率:{responseBodyJsonParse.records.location[0].weatherElement[1].time[0].parameter.parameterName}" + "%" +
-                                                               Environment.NewLine + $"最低溫度:{responseBodyJsonParse.records.location[0].weatherElement[2].time[0].parameter.parameterName}" + "°C" +
-                                                               Environment.NewLine + $"最高溫度:{responseBodyJsonParse.records.location[0].weatherElement[4].time[0].parameter.parameterName}" + "°C" +
-                                                               Environment.NewLine + $"天氣舒適度:{responseBodyJsonParse.records.location[0].weatherElement[3].time[0].parameter.parameterName}";
-                        Console.WriteLine("抓取天氣API成功!");
-
-                    }
-                    else if (response.IsSuccessStatusCode == false)
-                    {
-                        Console.WriteLine("抓取天氣API失敗!");
-                        return result;
-                    }
-                    return result;
-                }
-                catch (HttpRequestException e)
-                {
-                    Console.WriteLine("抓取天氣失敗!");
-                    Console.WriteLine("Message :{0} ", e.Message.ToString());
-                    return "";
-                }
-
-            }
-            static async Task<List<CarouselColumnObjectDto>> GetWeatherCarousel(string localname)
-            {
-                try
-                {
-                    List<CarouselColumnObjectDto> carouselList = new List<CarouselColumnObjectDto>();
-
-                    string result = "";
-                    string Path = $"https://opendata.cwb.gov.tw/api/v1/rest/datastore/F-C0032-001?format=JSON&locationName={localname}";
-                    client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("CWB-99A47F28-FFB9-467F-B4E9-6972DDCF3CD6");
-                    using HttpResponseMessage response = await client.GetAsync(Path);
-                    //Console.WriteLine(response.ToString());
-
-                    if (response.IsSuccessStatusCode == true)
-                    {
-                        response.EnsureSuccessStatusCode();
-                        string responseBody = await response.Content.ReadAsStringAsync();
-                        WeatherReturn responseBodyJsonParse = JsonConvert.DeserializeObject<WeatherReturn>(responseBody);
-
-                        for (int number = 0; number < 3; number++)
-                        {
-                            CarouselColumnObjectDto carouselColumnObject = new CarouselColumnObjectDto();
-                            string? StartTime = Convert.ToDateTime(responseBodyJsonParse.records.location[0].weatherElement[0].time[number].startTime).ToString("MM/dd HH:mm");
-                            string? EndTime = Convert.ToDateTime(responseBodyJsonParse.records.location[0].weatherElement[0].time[number].endTime).ToString("MM/dd HH:mm");
-                            result = localname + ":" +
-                                      Environment.NewLine + $"天氣狀態: {responseBodyJsonParse.records.location[0].weatherElement[0].time[number].parameter.parameterName}" +
-                                      Environment.NewLine + $"降雨機率: {responseBodyJsonParse.records.location[0].weatherElement[1].time[number].parameter.parameterName}" + "%" +
-                                      Environment.NewLine + $"最低溫度: {responseBodyJsonParse.records.location[0].weatherElement[2].time[number].parameter.parameterName}" + "°C" +
-                                      Environment.NewLine + $"最高溫度: {responseBodyJsonParse.records.location[0].weatherElement[4].time[number].parameter.parameterName}" + "°C" +
-                                      Environment.NewLine + $"舒適度: {responseBodyJsonParse.records.location[0].weatherElement[3].time[number].parameter.parameterName}";
-                            //Console.WriteLine("抓取天氣API成功!");
-                            carouselColumnObject.Title = StartTime + "~" + EndTime;
-                            carouselColumnObject.Text = result;
-                            carouselList.Add(carouselColumnObject);
-                        }
-
-                    }
-                    else if (response.IsSuccessStatusCode == false)
-                    {
-                        Console.WriteLine("抓取天氣API失敗!");
-                        return null;
-                    }
-                    return carouselList;
-                }
-                catch (HttpRequestException e)
-                {
-                    Console.WriteLine("抓取天氣失敗!");
-                    Console.WriteLine("Message :{0} ", e.Message.ToString());
-                    return null;
-                }
-
+                Console.WriteLine("回復訊息失敗!\n" + ex.ToString());
             }
 
         }
+        static async Task<string> GetWeather()
+        {
+            try
+            {
+                string result = "";
+                string local = "新竹市";
+                string Path = $"https://opendata.cwb.gov.tw/api/v1/rest/datastore/F-C0032-001?format=JSON&locationName={local}";
+                client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("CWB-99A47F28-FFB9-467F-B4E9-6972DDCF3CD6");
+                using HttpResponseMessage response = await client.GetAsync(Path);
+                Console.WriteLine(response.ToString());
+
+                if (response.IsSuccessStatusCode == true)
+                {
+                    response.EnsureSuccessStatusCode();
+                    string responseBody = await response.Content.ReadAsStringAsync();
+                    WeatherReturn responseBodyJsonParse = JsonConvert.DeserializeObject<WeatherReturn>(responseBody);
+                    var StartTime = Convert.ToDateTime(responseBodyJsonParse.records.location[0].weatherElement[0].time[1].startTime).ToString("yyyy  MM / dd dddd HH:mm");
+                    var EndTime = Convert.ToDateTime(responseBodyJsonParse.records.location[0].weatherElement[0].time[1].endTime).ToString("yyyy  MM / dd dddd HH:mm");
+                    result = "新竹市12小時天氣預報" +
+                                                           Environment.NewLine + $"{StartTime}" +
+                                                           Environment.NewLine + $"{EndTime}" +
+                                                           Environment.NewLine + $"天氣狀態:{responseBodyJsonParse.records.location[0].weatherElement[0].time[0].parameter.parameterName}" +
+                                                           Environment.NewLine + $"降雨機率:{responseBodyJsonParse.records.location[0].weatherElement[1].time[0].parameter.parameterName}" + "%" +
+                                                           Environment.NewLine + $"最低溫度:{responseBodyJsonParse.records.location[0].weatherElement[2].time[0].parameter.parameterName}" + "°C" +
+                                                           Environment.NewLine + $"最高溫度:{responseBodyJsonParse.records.location[0].weatherElement[4].time[0].parameter.parameterName}" + "°C" +
+                                                           Environment.NewLine + $"天氣舒適度:{responseBodyJsonParse.records.location[0].weatherElement[3].time[0].parameter.parameterName}";
+                    Console.WriteLine("抓取天氣API成功!");
+
+                }
+                else if (response.IsSuccessStatusCode == false)
+                {
+                    Console.WriteLine("抓取天氣API失敗!");
+                    return result;
+                }
+                return result;
+            }
+            catch (HttpRequestException e)
+            {
+                Console.WriteLine("抓取天氣失敗!");
+                Console.WriteLine("Message :{0} ", e.Message.ToString());
+                return "";
+            }
+
+        }
+        static async Task<List<CarouselColumnObjectDto>> GetWeatherCarousel(string localname)
+        {
+            try
+            {
+                List<CarouselColumnObjectDto> carouselList = new List<CarouselColumnObjectDto>();
+
+                string result = "";
+                string Path = $"https://opendata.cwb.gov.tw/api/v1/rest/datastore/F-C0032-001?format=JSON&locationName={localname}";
+                client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("CWB-99A47F28-FFB9-467F-B4E9-6972DDCF3CD6");
+                using HttpResponseMessage response = await client.GetAsync(Path);
+                //Console.WriteLine(response.ToString());
+
+                if (response.IsSuccessStatusCode == true)
+                {
+                    response.EnsureSuccessStatusCode();
+                    string responseBody = await response.Content.ReadAsStringAsync();
+                    WeatherReturn responseBodyJsonParse = JsonConvert.DeserializeObject<WeatherReturn>(responseBody);
+
+                    for (int number = 0; number < 3; number++)
+                    {
+                        CarouselColumnObjectDto carouselColumnObject = new CarouselColumnObjectDto();
+                        string? StartTime = Convert.ToDateTime(responseBodyJsonParse.records.location[0].weatherElement[0].time[number].startTime).ToString("MM/dd HH:mm");
+                        string? EndTime = Convert.ToDateTime(responseBodyJsonParse.records.location[0].weatherElement[0].time[number].endTime).ToString("MM/dd HH:mm");
+                        result = localname + ":" +
+                                  Environment.NewLine + $"天氣狀態: {responseBodyJsonParse.records.location[0].weatherElement[0].time[number].parameter.parameterName}" +
+                                  Environment.NewLine + $"降雨機率: {responseBodyJsonParse.records.location[0].weatherElement[1].time[number].parameter.parameterName}" + "%" +
+                                  Environment.NewLine + $"最低溫度: {responseBodyJsonParse.records.location[0].weatherElement[2].time[number].parameter.parameterName}" + "°C" +
+                                  Environment.NewLine + $"最高溫度: {responseBodyJsonParse.records.location[0].weatherElement[4].time[number].parameter.parameterName}" + "°C" +
+                                  Environment.NewLine + $"舒適度: {responseBodyJsonParse.records.location[0].weatherElement[3].time[number].parameter.parameterName}";
+                        //Console.WriteLine("抓取天氣API成功!");
+                        carouselColumnObject.Title = StartTime + "~" + EndTime;
+                        carouselColumnObject.Text = result;
+                        carouselList.Add(carouselColumnObject);
+                    }
+
+                }
+                else if (response.IsSuccessStatusCode == false)
+                {
+                    Console.WriteLine("抓取天氣API失敗!");
+                    return null;
+                }
+                return carouselList;
+            }
+            catch (HttpRequestException e)
+            {
+                Console.WriteLine("抓取天氣失敗!");
+                Console.WriteLine("Message :{0} ", e.Message.ToString());
+                return null;
+            }
+
+        }
+
+        public string getPostgresDate()
+        {
+            try
+            {
+                string Host = "soulkeydb.fly.dev";
+                string User = "postgres";
+                string DBname = "linebotapp";
+                string Password = "NeB8A71EYRW4CgU";
+                string Port = "5432";
+                string connString =
+                String.Format(
+                   "Host={0};Username={1};Database={2};Port={3};Password={4};SSLMode=disable",
+                   Host,
+                   User,
+                   DBname,
+                   Port,
+                   Password);
+
+                using (var conn = new NpgsqlConnection(connString))
+                {
+                    conn.Open();
+                    Console.WriteLine("連線db成功!v6");
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("連線db失敗!v6");
+                Console.WriteLine(e.Message);
+                throw new ArgumentException(e.ToString());
+            }
+            return "";
+
+        }
+
     }
+}
+
 
